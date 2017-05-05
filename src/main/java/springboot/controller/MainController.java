c2f1cbdb3895bfa85784378938dba3622b82635a
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2017-05-06 01:47:41 +0200

    manuel injection ile listeleme yapıldı...

--- src/main/java/springboot/controller/MainController.java
+++ src/main/java/springboot/controller/MainController.java
@@ -1,10 +1,16 @@
 package springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import springboot.service.PictureService;
+
 @Controller
 public class MainController {
+	
+	@Autowired
+	private PictureService pictureService;
 
 	@GetMapping("/home")
 	public String home()
--- src/main/java/springboot/controller/SampleRestController.java
+++ src/main/java/springboot/controller/SampleRestController.java
@@ -1,15 +1,27 @@
 package springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import springboot.service.PictureService;
+
 @RestController
 public class SampleRestController {
+	
+	@Autowired
+	private PictureService pictureService;
 
 	
 	@GetMapping("/test")
 	public String test()
 	{
 		return "test basarili v1";
+	}
+	
+	@GetMapping("/all-pictures")
+	public String allPictures()
+	{
+		return pictureService.findAll().toString();
 	}
 }
