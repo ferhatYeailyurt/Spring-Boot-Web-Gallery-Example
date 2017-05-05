b30ab347799592b702892e3c07fda7dea0ec61cd
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2017-05-05 23:22:24 +0200

    Spring Boot işlemi ile ilgili test işlemi yapıldı.

---
+++ src/main/java/springboot/controller/SampleRestController.java
@@ -0,0 +1,15 @@
+package springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleRestController {
+
+	
+	@GetMapping("/test")
+	public String test()
+	{
+		return "test basarili v1";
+	}
+}
