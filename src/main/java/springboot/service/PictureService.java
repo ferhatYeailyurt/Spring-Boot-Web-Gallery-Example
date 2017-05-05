890d0b84df2c6600bfe0c032ac5e1e8741bb6ad7
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2017-05-06 01:05:53 +0200

    PictureService.class ' ında findAll() methodu ile kayıtlar listelendi.

---
+++ src/main/java/springboot/service/PictureService.java
@@ -0,0 +1,36 @@
+package springboot.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import springboot.dao.PictureRepository;
+import springboot.model.Picture;
+
+@Service
+@Transactional
+public class PictureService {
+	
+	private final PictureRepository pictureRepository;
+
+	public PictureService(PictureRepository pictureRepository) {
+		this.pictureRepository = pictureRepository;
+	}
+	
+	public List<Picture> findAll()
+	{
+		List<Picture> pictures=new ArrayList<>();
+		
+		for(Picture picture : pictureRepository.findAll())
+		{
+			pictures.add(picture);
+		}
+		return pictures;
+		
+		
+	}
+	
+}
