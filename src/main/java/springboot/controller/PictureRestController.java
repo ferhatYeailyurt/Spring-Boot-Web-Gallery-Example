6e20aab0753b5f68d3cf88b917f8e7faa06f1fc8
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2017-05-08 23:17:25 +0200

    Mapping işlemi düzeltildi ve resources/images klasörü altına dış ortamdan resimler kayıt oldu.

--- src/main/java/springboot/controller/PictureRestController.java
+++ src/main/java/springboot/controller/PictureRestController.java
@@ -2,10 +2,14 @@ package springboot.controller;
 
 import java.util.Date;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import springboot.model.Picture;
 import springboot.service.PictureService;
@@ -29,13 +33,13 @@ public class PictureRestController {
 		return pictureService.findAll().toString();
 	}
 	
-	@GetMapping("/save-picture")
+	/*@GetMapping("/save-picture")
 	public String savePicture(@RequestParam String name,@RequestParam String description )
 	{
 		Picture picture=new Picture(name, description, new Date());
 		pictureService.save(picture);
 		return "picture saved";
-	}
+	}*/
 	
 	@GetMapping("/delete-picture")
 	public String deletePicture(@RequestParam int id )
@@ -43,4 +47,6 @@ public class PictureRestController {
 		pictureService.delete(id);
 		return "delete picture";
 	}
+	
+	
 }
